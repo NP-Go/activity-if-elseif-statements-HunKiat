ef91a2d720b9d563dcdf742205bf45f9828c0f13
HunKiat <hunkiat@gmail.com>
2022-03-29 11:58:20 +0800

    added test if int is odd and lenght of nos

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func noCharLen(s string) int {
+	return len(s)
+}
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+	number1 := 17
+	number2 := 24
+	resultMessage := "The number is odd"
+
+	// check if number is odd using modulus reminder
+	if number1%2 == 0 {
+		fmt.Println("The number is even")
+	} else if number1%2 == 1 {
+		fmt.Println(resultMessage)
+	}
+
+	fmt.Println("The # of chars are: ", noCharLen(strconv.Itoa(number2)))
 }
